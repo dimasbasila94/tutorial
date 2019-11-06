50bd4610242925e3ae8f6a5aa14e98de94cb68fd
dimasbasila <rahmanjidimas@gmail.com>
2019-11-06 23:52:52 +0700

    first commit

---
+++ website/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>docker test</title>
+</head>
+<body>	
+	<h1>welcome lutvi</h1>
+	<ul>
+		<?php
+			$json = file_get_contents('http://product-service');
+			$obj = json_decode($json);
+
+			$products = $obj->products;
+			foreach ($products as $products) {
+				echo '<li>'.$products.'</li>';
+			}
+		?>
+	</ul>
+</body>
+</html>
